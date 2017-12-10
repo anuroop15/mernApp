069ef6e16ea994c8476295cb894468abc05293e4
anuroop <anuroop.buggaveeti15@gmail.com>
2017-12-09 22:08:06 -0800

    fixed server run  and webpack issues

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 //import AppRoutes from './components/AppRoutes';
 
 export default class HelloWord extends React.Component {
 	render () {
 		return (
 			<div>
-				Hello Word
+				Hello Anuroop
 			</div>
 		)
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -5,35 +5,31 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  devtool: debug ? 'inline-sourcemap' : null,	
+  //devtool: debug ? 'inline-sourcemap' : null,	
   entry: path.join(__dirname, 'src', 'app.js'),
   output: {
     path: path.join(__dirname, 'src', 'static', 'js'),
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ["es2016", "react"]
+    rules: [
+        {
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
         }
-      }
-      //loader: ['babel-loader'],
-      // query: {
-      //   cacheDirectory: 'babel_cache',
-      //   //presets: debug ? ['react', 'es2016', 'react-hmre'] : ['react', 'es2016']
-      //   presets: ['react', 'es2016']
-      // }
-    }]
+    ]
   },
   plugins: debug ? [] : [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    new webpack.optimize.DedupePlugin(),
+    //new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
